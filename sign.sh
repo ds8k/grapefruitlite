420de4efc54d879d87bfd9ad06bb1b22c2d4abfa
Cameron Bates <darksonic8000@gmail.com>
2015-02-11 17:16:09 -0500

    Ignore

--- sign.sh
+++
@@ -1,3 +0,0 @@
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore grapefruitfree.keystore platforms/android/ant-build/GrapefruitBlowsFree-release-unsigned.apk grapefruitfree
-rm -f platforms/android/ant-build/GrapefruitBlowsFree.apk
-zipalign -v 4 platforms/android/ant-build/GrapefruitBlowsFree-release-unsigned.apk platforms/android/ant-build/GrapefruitBlowsFree.apk
